2c2eb0e94272c3717d6a2acfd1008eb2c8233128
amachkel <amachkel@gmail.com>
2022-04-11 18:55:42 -0400

    admin authentication feature

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-
+const { users } = require('./userData');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
