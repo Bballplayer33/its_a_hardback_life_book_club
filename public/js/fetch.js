c57953fdb96d7e39715ed94072cfed1ca76c2978
Quadrilateral <amandacdulaney@gmail.com>
2022-04-13 19:05:36 -0400

    fixed getBooks image render

--- public/js/fetch.js
+++ public/js/fetch.js
@@ -1,21 +1,29 @@
-//Fetch data from OpenLibrary API and render to search.handlebars
-function getBooks() {
+// //Fetch data from OpenLibrary API and render to search.handlebars & main.handlebars
+window.onload = () => {
+const getBooks = async (event) => {
+    event.preventDefault();
     document.getElementById('output').innerHTML = '';
     console.log("Search clicked");
-    fetch('http://openlibrary.org/search.json?q=' + 
-    document.getElementById('input').value)
-    .then(a=>a.json())
-    .then(response=>{
-        for (var i=0; i<response.docs.length; i++){
-            document.getElementById('output').innerHTML += 
-            '<h2>'+ 
-            response.docssearch[i].title + 
-            '</h2>' + 
-            response.docs[i].author_name[0] + 
-            '<br><img src="http://covers.openlibrary.org/b/isbn/' +
-            response.docs[i].isbn[0] +
-            'M-jpg"><br>';
-        }
-    });
-  }
-  
+    const response = await
+    fetch('http://openlibrary.org/search.json?q=' +
+        document.getElementById('input').value)
+        .then(a => a.json())
+        .then(data => {
+            console.log(data);
+            for (var i = 0; i < 10; i++) {
+                document.getElementById('output').innerHTML +=
+                    '<h2>' +
+                    data.docs[i].title +
+                    '</h2>' +
+                    data.docs[i].author_name[0] 
+                    if (typeof data.docs[i].isbn[0] !== undefined) {
+                    document.getElementById('output').innerHTML +=
+                    '<br><img src="http://covers.openlibrary.org/b/isbn/' +
+                    data.docs[i].isbn[0] + '-M.jpg"><br>'
+                    };
+            }
+        });
+}
+
+document.getElementById("bookName").addEventListener("click", getBooks);
+}
